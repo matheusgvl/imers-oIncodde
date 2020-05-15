b326173be5022e9404781bfed15a7759891e3908
matheusgvl <matheusgvl@hotmail.com.br>
2020-05-15 16:28:55 -0300

    Create desafio 3 .js

---
+++ desafio 3/desafio 3 .js
@@ -0,0 +1,18 @@
+var saida;
+let x = 12;
+let y = 12;
+soma(y,x);   
+ function soma(a,b){
+     while(a==b){
+        saida=0;
+        break;
+     }
+    for(let i=a;i<b;i++){
+        //console.log(i);
+        i++;
+        var saida=i++;
+}
+
+console.log("o valor da saida é\n"+saida);
+}
+
