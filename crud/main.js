aa8262b372dc17ec7ad969f8f6a57b583d9c8588
matheusgvl <matheusgvl@hotmail.com.br>
2020-05-26 15:56:18 -0300

    Update main.js

--- crud/main.js
+++ crud/main.js
@@ -1,22 +1,35 @@
+
 let i=1;
 
-let tabelaUsuario= document.getElementById("users_name");
+let tabelaUsuario=document.getElementById("users_name");
 let aux= document.getElementById("incremento");
+
 let entradaUsuario= document.getElementById("input_users");
-let btnCadastrarUsuario= document.querySelector("#btn_user");
+let btnCadastrarUsuario= document.querySelector("#btn_users");
 
    
 btnCadastrarUsuario.addEventListener('click',event =>{
    //let tabela= document.createElement("table");
-  
+   
+   if(entradaUsuario.value == "")
+   {
+     alert("você não digitou o nome");
+   }
+  else {
+   
    let incrementar=document.createElement("tr");
-   let campoUsuario = document.createElement("tr");
+    
+
+   let campoUsuario= document.createElement("tr");
+   
    aux.appendChild(incrementar).innerHTML=i;
    i++;
   
-  tabelaUsuario.appendChild(campoUsuario).innerHTML=entradaUsuario.value;
-  
-
+   
+   tabelaUsuario.appendChild(campoUsuario).innerHTML=entradaUsuario.value;
+ 
+   
+   }
    
 });
 
@@ -31,11 +44,16 @@ btnCadastrarBooks.addEventListener('click',event =>{
   
    let incrementar2=document.createElement("tr");
    let campoBooks = document.createElement("tr");
+   if(entradaBooks.value == "")
+   {
+     alert("você não digitou o nome do livro");
+   }
+   else{
    aux2.appendChild(incrementar2).innerHTML=j;
    j++;
   
   tabelaBooks.appendChild(campoBooks).innerHTML=entradaBooks.value;
-  
+}
 
    
 });
