5a6bba636af6ecd62c0ddba527cfc7a5872c4f7f
matheusgvl <matheusgvl@hotmail.com.br>
2020-05-29 18:50:08 -0300

    Create header.jsx

---
+++ meu_app2/src/components/header/header.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './Header.css';
+
+export class header extends Component {
+    render() {
+        return (
+            <div className="header">
+            <a href="users">usuario</a>
+            <a href="google.com" id="img"></a>
+       
+             <a href="google">imagens</a>
+            
+             <a href="google.com">Gmail</a>
+             
+             
+             
+            
+            </div>
+        )
+    }
+}
+
+export default header
