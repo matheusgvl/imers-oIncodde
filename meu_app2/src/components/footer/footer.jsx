54fc72ebcd0af2919c93ecd6067d0599ae7533fe
matheusgvl <matheusgvl@hotmail.com.br>
2020-05-29 18:49:17 -0300

    Create footer.jsx

---
+++ meu_app2/src/components/footer/footer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './style.css';
+export class footer extends Component {
+    render() {
+        return (
+            <div className="rodape">
+                <a href="google.com">Publicidade</a>
+                <a href="google.com">Negocios</a>
+                <a href="google.com">Sobre</a>
+                <a href="google.com">Como funciona a pesquisa</a>
+                <a href="google.com">Privacidade</a>
+                <a href="google.com">termos</a>
+                <a href="google.com">configuração</a>
+            </div>
+        )
+    }
+}
+
+export default footer
